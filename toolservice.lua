db48260e8eb02fe3526088805dcc5b5d2630f8e0
guywithapoo <84512679+guywithapoo@users.noreply.github.com>
2022-04-28 14:31:54 -0400

    Create toolservice.lua
    ggg

---
+++ toolservice.lua
@@ -0,0 +1,10 @@
+
+
+function module.new(tool) -- tool OR table that includes all the info needed
+  -- "tool" module script inside tool that you search for and require if found, provides a new UI that u can use inside the tool UI as a custom thing
+  
+  
+  
+
+
+end
